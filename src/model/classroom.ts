468287eaa2b607b41ce70bec29d58f0f40c3e1a3
sirageDb <ilovesemicolons@hotmail.com>
2021-05-24 11:50:58 +0200

    created classroom model

---
+++ src/model/classroom.ts
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+
+const classroom = new mongoose.Schema({
+    name: String,
+    year: String,
+    student: [
+        {
+            firstname: String,
+            lastname: String,
+            mail: String,
+        }
+    ],
+    subject: [
+        {
+            flashcard: [
+                {
+                    title: String,
+                    tag: [],
+                    subtitle: [
+                        {
+                            title: String,
+                            position: Number,
+                            paragraph: [
+                                {
+                                    text: String,
+                                    isValidate: Boolean,
+                                    isPublic: Boolean,
+                                    author: String,
+                                    date: Date
+                                }
+                            ]
+                        },
+                    ],
+                    ressource: [
+                        {
+                            name: String,
+                            url: String
+                        }
+                    ],
+                    question: [
+                        {
+                            text: String,
+                            answer: [
+                                {
+                                    text: String,
+                                    author: String,
+                                    date: Date
+                                }
+                            ],
+                            date: Date,
+                            author: String,
+                            isPublic: Boolean
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+});
