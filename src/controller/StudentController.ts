ada3625ea79c13c9c89b315a3358b63027d6bc05
SirageDb <ilovesemicolons@hotmail.com>
2021-06-24 12:10:21 +0200

    classroom controller

---
+++ src/controller/StudentController.ts
@@ -0,0 +1,14 @@
+/* import userModel from '../model/user';
+import classroom from '../model/classroom';
+import { Mutation, Resolver } from 'type-graphql';
+
+@Resolver(classroom)
+export default class ClassRoomController {
+  @Mutation((returns) => Student)
+  public async addStudent() {}
+}
+ */
+
+
+//TODO check if student already exists
+//TODO validate email format
