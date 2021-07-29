4521f4f8390997b816131218ab28d53d36014085
sirageDb <ilovesemicolons@hotmail.com>
2021-07-29 11:02:30 +0200

    + new file structure inorder to have a testing enironnement

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,30 @@
-describe('basic test suite', () => {
-  it('test', () => {
-    expect(true).toBeTruthy();
+import "reflect-metadata";
+import startServer from "./server"; //get only start server function
+import config from "./config/env.testing"; //get testing environnement
+import { gql } from "apollo-server-core";
+import mongoose from "mongoose";
+import { ApolloServer } from "apollo-server-express";
+import { MongoMemoryServer } from "mongodb-memory-server"; //spinning mongo in memory for fast tests
+import createTestClient from "apollo-server-testing";
+
+
+/// resolver
+const GET_ALL_CLASSROOMS = gql`{getAllClassrooms{_id, name, year}}`;
+
+
+describe(
+  'basic test suite',
+  () => {
+    let apollo: ApolloServer | null = null;
+    let mongo: MongoMemoryServer = new MongoMemoryServer();
+
+    beforeAll(
+      async () => {
+        mongo = new MongoMemoryServer();
+        config.db = mongo.getUri();
+
+        apollo = await startServer(config);
+      }
+    )
+
   });
-});
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,8 @@
 import 'reflect-metadata';
-import cors from 'cors';
-import express from 'express';
-import morgan from 'morgan';
 import dotenv from 'dotenv';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
-import { GraphQLSchema } from 'graphql';
-import mongoose from 'mongoose';
-import config from '../.env.dev';
-import UserAuthResolver from './controller/UserAuthResolver';
-import ClassroomResolver from './controller/ClassroomResolver';
+import config from './config/env.dev';
+import startServer from './server';
 
 dotenv.config();
-const initialize = async () => {
-  //Connect to database===================================
-  mongoose
-    .connect(config.db, config.options)
-    .then(() => {
-      console.log('MongoDb started');
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  //Connect to database===================================
 
-  const app: express.Application = express();
-
-  //TODO do not forget to not allow * here
-  app.use(cors({ origin: '*' }));
-  app.use(express.json());
-
-  //keep the extended true, otherwise you will get deprecated warnings
-  app.use(express.urlencoded({ extended: true }));
-
-  app.use(morgan('dev'));
-  const schema: GraphQLSchema = await buildSchema({
-    validate: false,
-    resolvers: [UserAuthResolver, ClassroomResolver],
-  });
-
-
-  const server = new ApolloServer({ schema });
-
-  await server.start();
-  server.applyMiddleware({ app, path: '/' });
-  app.listen(4000, () => {
-    console.log('launch');
-  });
-  console.log('Server Apollo Running on http://localhost:4000 ');
-};
-
-initialize();
+startServer(config)
