da0b33f8be282ecddea5181a1d0a12ea1aaf024a
SirageDb <ilovesemicolons@hotmail.com>
2021-11-04 19:36:30 +0100

    possibility to create a paragraph by providing, classroomId, subjectId, flashcardId, subtitleId, paragraph texx/author/ispublic

--- src/controller/FlashcardResolver.ts
+++ src/controller/FlashcardResolver.ts
@@ -13,11 +13,11 @@ import {
 import { ApolloError } from 'apollo-server-express';
 import mongoose, { Aggregate } from 'mongoose';
 import ClassroomModel from '../model/classroom';
-import FlashcardModelGQL, {
-  Ressource,
-  Subtitle,
-} from '../model/graphql/flashcardModelGQL';
 import { ITokenContext } from '../utils/interface';
+import classroomModel from '../model/classroom';
+import FlashcardModelGQL, { Paragraph, Ressource, Subtitle } from '../model/graphql/flashcardModelGQL';
+import getCurrentLocalDateParis from '../utils/getCurrentLocalDateParis';
+
 
 
 
@@ -35,11 +35,11 @@ class CreateFlahscard implements Partial<FlashcardModelGQL>  {
   @Field((type) => [String])
   tag!: string[];
 
-  @Field((type) => [CreateSubtitle])
-  subtitle!: CreateSubtitle[];
+  @Field((type) => [SubtitleInput])
+  subtitle!: SubtitleInput[];
 
-  @Field((type) => [CreateRessource])
-  ressource!: CreateRessource[];
+  @Field((type) => [RessourceInput])
+  ressource!: RessourceInput[];
 }
 
 
@@ -61,15 +61,73 @@ class UpdateFlashcard implements Partial<FlashcardModelGQL> {
   @Field((type) => [String], { nullable: true })
   tag: string[] | undefined;
 
-  @Field((type) => [CreateSubtitle], { nullable: true })
-  subtitle: CreateSubtitle[] | undefined;
+  @Field((type) => [SubtitleInput], { nullable: true })
+  subtitle: SubtitleInput[] | undefined;
 
-  @Field((type) => [CreateRessource], { nullable: true })
-  ressource: CreateRessource[] | undefined
+  @Field((type) => [RessourceInput], { nullable: true })
+  ressource: RessourceInput[] | undefined
 }
 
+
 @InputType()
-class CreateRessource extends Ressource {
+class ParagraphInput implements Partial<Paragraph> {
+  @Field()
+  text!: string;
+
+  @Field()
+  isPublic!: boolean;
+
+  @Field()
+  author!: string;
+}
+
+
+@ArgsType()
+class CreateParagraph extends FlashcardModelGQL {
+
+  @Field((type) => ID)
+  classroomId!: string;
+
+  @Field((type) => ID)
+  subjectId!: string;
+
+  @Field((type) => ID)
+  flashcardId!: string;
+
+  @Field((type) => ID)
+  subtitleId!: string
+
+  @Field((type) => ParagraphInput)
+  paragraph!: ParagraphInput;
+}
+
+
+
+
+@ArgsType()
+class UpdateParagraph extends FlashcardModelGQL {
+  @Field((type) => ID)
+  classroomId!: string;
+
+  @Field((type) => ID)
+  subjectId!: string;
+
+  @Field((type) => ID)
+  flashcardId!: string;
+
+  @Field((type) => ID)
+  subtitleId!: string
+
+  @Field((type) => [ParagraphInput])
+  paragraph!: ParagraphInput[];
+}
+
+
+
+
+
+@InputType()
+class RessourceInput extends Ressource {
   @Field()
   name!: string;
 
@@ -78,14 +136,14 @@ class CreateRessource extends Ressource {
 }
 
 @InputType()
-class CreateSubtitle extends Subtitle {
+class SubtitleInput extends Subtitle {
   @Field()
   title!: string;
 
   @Field()
   position!: number;
 }
-
+// {{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}
 @ArgsType()
 class CreateFlahsCard implements Partial<FlashcardModelGQL> {
   @Field()
@@ -107,8 +165,53 @@ class CreateFlahsCard implements Partial<FlashcardModelGQL> {
   ressource!: CreateRessource[];
 }
 
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
 @Resolver(FlashcardModelGQL)
 export default class FlashcardResolver {
+
+  // Private method to get a classroom by providing its id
+  //= ================================================
+  private async getClassroomById(classroomId: string) {
+    try {
+      const classroom = await classroomModel.findOne({ _id: classroomId });
+      return classroom
+    }
+    catch {
+      throw new ApolloError("cannot find classroom")
+    }
+  }
+
+  // private method to get subject located in a classroom by providing classroom object and subjectId
+  //= ================================================
+  private getSubjectById(classroom: any, subjectId: string) {
+    const subject = classroom.subject.find(
+      (currentSubject: any) => currentSubject._id == subjectId
+    )
+    return subject;
+  }
+
+  // private method to get a flashcard by providing subject object and flashcard id
+  //= ================================================
+  private getFlashcardById(subject: any, flashcardId: string) {
+    const flashcard = subject.flashcard.find(
+      (currentFlashcard: any) => currentFlashcard._id == flashcardId
+    )
+    return flashcard;
+  }
+
+  // private method to get a flashcard by providing subject object and flashcard id
+  //= ================================================
+  private getSubtitleById(flashcard: any, subtitleId: string) {
+    const subtitle = flashcard.subtitle.find(
+      (currentSubtitle: any) => currentSubtitle._id == subtitleId
+    )
+    return subtitle;
+  }
+
+
   @Query(() => [FlashcardModelGQL])
   public async getAllFlashcards(
     @Arg('classroomId') classroomId: string,
@@ -117,7 +220,6 @@ export default class FlashcardResolver {
     const { user } = ctx;
     console.log(user.id);
     const classroom = await ClassroomModel.findById(classroomId);
-
     if (!classroom) {
       throw new ApolloError('Classroom not found');
     }
@@ -180,9 +282,9 @@ export default class FlashcardResolver {
     return flashcard;
   }
 
-  // made for test and add quickly flashcard, return is not correct
+  // Create a flashcard
   // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-  @Mutation(() => FlashcardModelGQL)
+  @Mutation((returns) => FlashcardModelGQL)
   public async createFlashcard(
     @Args()
     {
@@ -267,20 +369,9 @@ export default class FlashcardResolver {
   ) {
 
     try {
-      const classroom = await ClassroomModel.findOne(
-        {
-          _id: classroomId,
-        },
-      )
-
-      const subject = classroom.subject.find(
-        (currentSubject: any) => currentSubject.subjectId == subjectId
-      )
-
-      const flashcard = subject.flashcard.find(
-        (currentFlashcard: any) => currentFlashcard._id == flashcardId
-      )
-
+      const classroom = await this.getClassroomById(classroomId);
+      const subject = this.getSubjectById(classroom, subjectId);
+      const flashcard = this.getFlashcardById(subject, flashcardId);
       title && (flashcard.title = title);
       ressource && (flashcard.ressource = ressource)
       tag && (flashcard.tag = tag)
@@ -293,5 +384,117 @@ export default class FlashcardResolver {
       throw new ApolloError("Error updating flashcard")
     }
   }
+
+
+  //  Create a paragraph by providing, classroomId, subjectId, flashcardId, subtitleId, paragraph texx/author/ispublic
+  // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  @Mutation((returns) => FlashcardModelGQL)
+  public async createParagraph(
+    @Args() {
+      classroomId,
+      subjectId,
+      flashcardId,
+      subtitleId,
+      paragraph
+    }
+      : CreateParagraph
+  ) {
+
+    try {
+      const classroom = await this.getClassroomById(classroomId);
+      const subject = this.getSubjectById(classroom, subjectId);
+      const flashcard = this.getFlashcardById(subject, flashcardId);
+      const subtitle = this.getSubtitleById(flashcard, subtitleId);
+
+      const createdParagraph = {
+        text: paragraph.text,
+        isValidated: false,
+        isPublic: paragraph.isPublic,
+        author: paragraph.author,
+        date: getCurrentLocalDateParis()
+      }
+
+      subtitle.paragraph = subtitle.paragraph.push(createdParagraph);
+      await classroomModel.updateOne(classroom);
+
+      return flashcard;
+    }
+    catch {
+      throw new ApolloError("Could not create a new paragraph")
+    }
+  }
+
+
+
+
+
+  //  update a paragraph by providing 
+  // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  /*   @Mutation((returns) => FlashcardModelGQL)
+    public async updateParagraph(
+      @Args() {
+        classroomId,
+        subjectId,
+        flashcardId,
+        subtitleId,
+        paragraph
+      }
+        : UpdateParagraph
+    ) {
+  
+  
+      const classroom = await this.getClassroomById(classroomId);
+      const subject = this.getSubjectById(classroom, subjectId);
+      const flashcard = this.getFlashcardById(subject, flashcardId);
+  
+      
+    
+    } */
+
+
+  /*   @Mutation((returns) => FlashcardModelGQL)
+    public async updateFlashcardParagraph(
+      @Arg('classroomId') classroomId: string,
+      @Arg('subjectId') subjectId : string,    
+      @Arg('flashcardId') flashcardId: string,
+      @Arg('paragraph') paragraph : Paragraph
+    ) {
+    } */
 }
 
+/* {
+  "subtitle_id" : [ { "paragraph_id" : "545",  "text" : "Update", "isPublic" : true}, { "paragraph_id" : "545",  "text" : "New", "isPublic" : true} ]
+  } */
+
+
+
+  // C'étais dans la mutation de createFlashcard
+  //= ===========================================================
+      // je laisse ça ici pour continuer de chercher sur du temps libre
+
+    // const class2 = await classroomModel
+    //   .aggregate()
+    //   .match({
+    //     _id: mongoose.Types.ObjectId(classroomId),
+    //     'subject.flashcard': {
+    //       $elemMatch: { _id: mongoose.Types.ObjectId(flashcardId) },
+    //     },
+    //   })
+    //   .unwind('$subject')
+    //   .unwind('$subject.flashcard')
+    //   .match({
+    //     'subject.flashcard._id': mongoose.Types.ObjectId(flashcardId),
+    //   })
+    //   .group({
+    //     _id: '$subject.flashcard._id',
+    //     title: { $first: '$subject.flashcard.title' },
+    //     tag: { $first: '$subject.flashcard.tag' },
+    //     subtitle: { $first: '$subject.flashcard.subtitle' },
+    //     ressource: { $first: '$subject.flashcard.ressource' },
+    //     question: { $first: '$subject.flashcard.question' },
+    //   })
+    //   .exec();
+    // console.log(class2);
+    // return class2;
+
+  //= ===========================================================
