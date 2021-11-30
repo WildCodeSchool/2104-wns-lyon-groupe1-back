9c034b902df535296c8e88305a63b765cb64a0f3
sirageDb <ilovesemicolons@hotmail.com>
2021-11-30 16:04:37 +0100

    + user model now using types

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { iUser } from "../utils/types/userTypes";
 
 const user = new mongoose.Schema({
     mail: {type : String, unique : true},
@@ -16,5 +17,5 @@ const user = new mongoose.Schema({
     ]
 });
 
-const model : mongoose.Model<any>= mongoose.model("users", user);
+const model : mongoose.Model<iUser>= mongoose.model("users", user);
 export default model;
