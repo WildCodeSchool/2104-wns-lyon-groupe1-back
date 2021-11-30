d52506f009a374231f1846b5d187f4d3282bf108
sirageDb <ilovesemicolons@hotmail.com>
2021-11-30 16:01:38 +0100

    + new types for mongo

---
+++ src/utils/types/classroomTypes.ts
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+
+export interface iParagraph{
+    _id? : string,
+    text : string,
+    isValidate : boolean,
+    isPublic : boolean,
+    author : string,
+    date : Date
+}
+
+export interface iRessource {
+    name : string,
+    url : string
+}
+
+export interface iSubtitle {
+    _id? : string,
+    title : string,
+    position : number,
+    paragraph : iParagraph[]
+}
+
+export interface iAnswer{
+    text : string,
+    author : string,
+    date : Date
+}
+
+export interface iQuestion{
+    text : string,
+    answer : iAnswer[]
+    date : Date,
+    author : string,
+    isPublic  : boolean
+}
+
+
+export interface iFlashcard {
+    _id? : string
+    title : string,
+    tag : string[]
+    subtitle  : iSubtitle[]
+    ressource : iRessource[]
+    question : iQuestion[]
+}
+
+export interface iSubject {
+    _id? : string,
+    subjectId : string,
+    flashcard : iFlashcard[]
+}
+
+export interface iStudent {
+    firstname: string,
+    lastname: string,
+    mail: string,
+    userId: string,
+}
+
+
+export interface iClassroom {
+    _id? : string,
+    name : string,
+    year : string,
+    student : iStudent[],
+    subject : iSubject[],
+}
